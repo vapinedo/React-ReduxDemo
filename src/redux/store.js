f0e6cb094a7602498b241306578488973179c0e1
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-06 13:57:42 -0500

    [ADD] Redux Devtool Extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware } from "redux"
 import logger from 'redux-logger'
 import { rootReducer } from "./rootReducer"
+import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(rootReducer, applyMiddleware(logger))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
