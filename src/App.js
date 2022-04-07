dee969a63c2536956c027a96d2e24a1629f50acf
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-07 12:49:33 -0500

    [ADD]  mapStateToProps

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import CakeContainer from './components/CakeContainer'
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <CakeContainer />
         <HooksCakeContainer />
         <IceCreamContainer />
